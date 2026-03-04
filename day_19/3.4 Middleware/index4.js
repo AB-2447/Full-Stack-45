3cd86d7b23f418713c53891bb475c2574b5662d1
Anshul Bhandwalkar <codewanshul@gmail.com>
2026-03-04 23:13:26 +0530

    Band name generator

--- day_19/3.4 Middleware/index4.js
+++ day_19/3.4 Middleware/index4.js
@@ -1,8 +1,32 @@
 import express from "express";
+import {dirname} from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
 
-const app = express();
-const port = 3000;
+const app=express()
+const port=3000
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+const __dirname = dirname(fileURLToPath(import.meta.url))
+var bandName="";
+
+app.use(express.urlencoded({ extended: true }));
+
+function bandNameGenerator(req,res,next){
+  console.log(req.body)
+  bandName=(req.body["street"] + req.body["pet"])
+  next()
+}
+
+app.use(bandNameGenerator);
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname + "/public/index.html")
 });
+
+app.post("/submit",(req,res)=>{
+  res.send(`<h1>Your Band name is ${bandName}</h1>`)
+})
+
+app.listen(port,()=>{
+  console.log(`Port running on ${port}`)
+})
