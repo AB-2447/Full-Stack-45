1113e6fd7f31091a06640ce4b1abf6b797f9e1ab
Anshul Bhandwalkar <codewanshul@gmail.com>
2026-07-01 19:37:50 +0530

    Drum project

---
+++ day_21/Drum Kit Starting Files/index.js
@@ -0,0 +1,16 @@
+var drums = document.querySelectorAll(".drum");
+var aud= new Audio("sounds/tom-1.mp3")
+
+for (var i = 0; i < drums.length; i++) { 
+    drums[i].addEventListener("click", function() {
+        switch (key) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+        
+    });
+}
